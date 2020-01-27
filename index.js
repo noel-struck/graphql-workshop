80279ef7bcb17092c65c225877c668f488d5fda5
Noel Flores <noel.flores@struck.com>
2020-01-27 08:57:40 -0600

    agregar variables de entorno

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { gql, ApolloServer } = require('apollo-server-express');
 
+// Call environment variables
+require('dotenv').config()
+
 // SERVER SETUP
 function startServer() {
     const app = express();
-    const PORT = 3000;
+    const PORT = process.env.PORT;
 
     app.listen(PORT, () => {
         console.log(`Server listening at port: ${PORT}`);
